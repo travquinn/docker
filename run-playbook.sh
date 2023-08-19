f4aeca459472dd4ef68bfb46aaa9f25b769d3830
travquinn <53845837+travquinn@users.noreply.github.com>
2023-08-19 22:24:08 +1000

    Update run-playbook.sh

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Change directory to where you want to store the playbook
-cd /path/to/ansible/playbook
+cd /opt
 
 # Run ansible-pull to execute the playbook
 ansible-pull -U https://github.com/travquinn/docker/blob/main/playbook.yml playbook.yml
